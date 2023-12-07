d670d8b75609207fcdedfeba382d7976c5629b03
Nimish <msnimish24@gmail.com>
2023-12-07 06:38:21 +0530

    Strings Done. Sorting started

--- Strings/src/App.java
+++ Strings/src/App.java
@@ -1,9 +1,46 @@
 public class App {
     public static void main(String[] args) throws Exception {
         // System.out.println("Hello, World!");
-        ReverseVowels("Done");
-        PrintConsonants("welcometoscaler");
-        ConvertUpperCaseToLowerCase("HiCaTherIneGooDMorNIng");
+        // ReverseVowels("Done");
+        // PrintConsonants("welcometoscaler");
+        // ConvertUpperCaseToLowerCase("HiCaTherIneGooDMorNIng");
+        String A = "Apple a day hegde";
+        // int B = 2;
+        // RemoveDupsOfLengthB(A, B);
+        ReverseWordsInPhrase(A);
+    }
+
+    public static void ReverseWordsInPhrase(String A){
+        String[] words = A.split("\\s+");
+        int N = words.length;
+        for(int i=0;i<Math.floor(N/2);i++){
+            String temp = words[i];
+            words[i] = words[N-i-1];
+            words[N-i-1] = temp;
+        }
+        String ans = "";
+        for(String word : words){
+           ans+=word+" ";
+        }
+        System.out.println(ans);
+    }
+
+    public static void RemoveDupsOfLengthB(String A, int B){ // Consecutive Duplicates are removed
+        char[] ch = A.toCharArray(); 
+        String str = "";
+        for (int i = 0; i < ch.length; i++) {
+            String subStr = ch[i] + "";
+            int j = i + 1;
+            while (j < ch.length && ch[i] == ch[j]) {
+                subStr += ch[j];
+                j++;
+            }
+            i = j - 1; // Adjust the loop variable
+            if (subStr.length() != B) {
+                str += subStr; // Correct the variable name
+            }
+        }
+        System.out.println(str);
     }
 
     public static void ConvertUpperCaseToLowerCase(String str){
