9c17debd18518093512133eec0a72162fe791a2d
Nimish <msnimish24@gmail.com>
2023-12-09 19:06:36 +0530

    Recursion Continued...

--- Recursion/src/App.java
+++ Recursion/src/App.java
@@ -1,8 +1,9 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("RECURSION......");
-        // int[] arr = {3,7,1,4,9,3,0}
-        printNumbersTill1Recursive(12);
+        int[] arr = {3,7,1,4,9,3,0};
+        // printNumbersTill1Recursive(12);
+        printArray(arr, 0);
     }
 
     public static void printNumbersTill1Recursive(int N){
@@ -14,5 +15,16 @@ public class App {
         // Main Logic
         printNumbersTill1Recursive(N-1);        // Recursive call
         System.out.print(N+" ");
+    }
+
+
+    public static void printArray(int[] arr, int i){
+        // Base Case
+        if(i == arr.length){
+            return;
+        }
+        // Main Logic
+        System.out.print(arr[i]+" ");
+        printArray(arr, i+1);
     }
 }
