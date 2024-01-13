fc62a5c5234bf6006fdb21feb5be29a1fb59543f
Nimish <msnimish24@gmail.com>
2024-01-13 17:39:52 +0530

    Added Hashmap

---
+++ Hashmap/src/App.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hashmap");
+        HashMap<String, Integer> population = new HashMap<>();
+
+        // add key-value pairs to Hashmap
+        population.put("India",138);
+        population.put("USA",33);
+        population.put("Australia",4);
+        population.put("Nepal",2);
+        population.put("Pakistan",22);
+
+        System.out.println(population.toString());
+
+        // get value of specific key from HashMap
+        System.out.println(population.get("USA"));
+        // population.
+    }
+}
