4510270981cf45f977a9827f89324b1c20319da3
Nimish <msnimish24@gmail.com>
2024-01-14 10:48:02 +0530

    HAshmap Done

--- Hashmap/src/App.java
+++ Hashmap/src/App.java
@@ -21,5 +21,44 @@ public class App {
         System.out.println(population.containsValue(31));
 
         System.out.println(population.toString());
+        maxCharFreq("swallow");
+        int[] a = {3,6,2,6,8,9};
+        int[] b = {4,7,2,8,1,3};
+        getCommonElements(a, b);
+    }
+
+    public static void maxCharFreq(String st){
+        System.out.println(st);
+        StringBuilder s = new StringBuilder(st);
+        HashMap<Character, Integer> hm = new HashMap<Character, Integer>();
+        for(int i = 0; i < s.length();i++){
+            if(hm.containsKey(s.charAt(i))){
+                hm.replace(s.charAt(i), hm.get(s.charAt(i)) + 1);
+            }else{
+                hm.put(s.charAt(i),1);
+            }
+        }
+        System.out.println(hm.toString());
+        int max = Integer.MIN_VALUE;
+        char maxChar = '_';
+        for(Character c : hm.keySet()){
+            if(max < hm.get(c)){
+                max = hm.get(c);
+                maxChar = c;
+            }
+        }
+        System.out.println("Max Char is '"+ maxChar+ "' which occurrs " + max + " times." );
+    }
+
+    public static void getCommonElements(int[] arr1, int[] arr2){
+        HashMap<Integer, Integer> hm = new HashMap<Integer, Integer>();
+        for(int i=0; i<arr1.length;i++){
+            hm.put(arr1[i],1);
+        }
+        for(int i=0; i<arr2.length;i++){
+            if(hm.containsKey(arr2[i])){
+                System.out.println(arr2[i]);
+            }
+        }
     }
 }
